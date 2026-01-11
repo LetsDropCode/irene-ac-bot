85a4bf3d81c1ddc2ceb248d88e4ffe61a4a3ed80
Lindsay Bull <lindsaybull@Lucians-MacBook-Air.local>
2026-01-11 22:22:13 +0200

    Export Submission model properly

---
+++ app/models/__init__.py
@@ -0,0 +1,11 @@
+from app.models.submission import Submission
+from app.models.member import Member
+from app.models.event import Event
+from app.models.event_code import EventCode
+
+__all__ = [
+    "Submission",
+    "Member",
+    "Event",
+    "EventCode",
+]
